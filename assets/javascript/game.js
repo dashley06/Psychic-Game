8ce966d745d73059a6007d4f03a24be02d7a278a
Ashley Clark <debashley06@gmail.com>
2019-09-04 18:25:38 -0400

    html css and pseudocode for word game

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+/*variables: computerWordLetter, computerWord, userGuess, userWins, userWrong, userGuesses, lettersGuessed, 
+            displayCorrectAnswer,
+
+UserGuesses=10
+UserWins=0
+
+//"Press any key to begin" as event.key to start ->store data as var userGuess
+//run funtion (computer randomly selects word/letters) and store as computerWord and the computerWordletter
+//if userGuess === computerWordLetter, then displayCorrectAnswer on browser
+        //clear userGuess var
+        //run event.key of user pressing button
+        //store new data as userGuess
+//if userGuess !== computerWordLetter, then UserGuesses -1 AND lettersGuessed (userGuess) display on browser
+        //clear userguess var
+        //run event.key of user pressing button
+        //store as userGuess
+
+//if computerWord === displayCorrectAnswer, then userWins + 1 and clear screen and start from beginning prompt
+    //userWins remains constant
+//if userGuesses === 0, clear screen
+    //userWins remains constant
