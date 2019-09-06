d57195ccefd3eda965ae4c0fc4e35848f031360a
Ashley Clark <debashley06@gmail.com>
2019-09-06 15:45:12 -0400

    switched assignment from word guess game to psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,44 @@
-/*variables: computerWordLetter, computerWord, userGuess, userWins, userWrong, userGuesses, lettersGuessed, 
-            displayCorrectAnswer,
 
-UserGuesses=10
-UserWins=0
 
-//"Press any key to begin" as event.key to start ->store data as var userGuess
-//run funtion (computer randomly selects word/letters) and store as computerWord and the computerWordletter
-//if userGuess === computerWordLetter, then displayCorrectAnswer on browser
+var directionsText = document.getElementById("directions-text");
+var userchoiceText = document.getElementById("userchoice-text"); //each letter of the random computer word
+var computerchoiceText= document.getElementById("cmputerchoice-text"); // user presses key which is stored in this variable
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var lettersGuessed= document.getElementById("lettersGuessed");
+var userWins = 0;
+var userLoss= 0;
+var lettersGuessed= []; //hold each letter  
+
+var clearGame = false;
+var computerChoices = ["enter all the letters"]
+
+//start game
+function setup(){
+//pick letter randomly
+var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+
+ //"Press any key to begin" as event.key to start
+ document.onkeyup = function(event){ 
+
+    var userGuess = event.key; //store data as var userGuess
+    
+}
+
+function checkLetter(userGuess){ //run funtion to check answer
+            if (userGuess === computerLetter[i]){
+                
+                userWins++;
+                winsText.textContent = 
+            }
+
+}
+    
+    
+    
+    
+    
+    //if userGuess === computerWordLetter, then displayCorrectAnswer on browser
         //clear userGuess var
         //run event.key of user pressing button
         //store new data as userGuess
@@ -18,4 +50,4 @@ UserWins=0
 //if computerWord === displayCorrectAnswer, then userWins + 1 and clear screen and start from beginning prompt
     //userWins remains constant
 //if userGuesses === 0, clear screen
-    //userWins remains constant
+    //userWins remains constant*/
