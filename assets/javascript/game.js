3ebf578749befcc7d11eb717dbfa922a8213cf79
Ashley Clark <debashley06@gmail.com>
2019-09-10 17:49:30 -0400

    psychic game homework edits to java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 
 var directionsText = document.getElementById("directions-text");
 var userchoiceText = document.getElementById("userchoice-text");
-var computerchoiceText= document.getElementById("cmputerchoice-text");
+var computerchoiceText= document.getElementById("cpmputerchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var lettersGuessed_char= document.getElementById("lettersGuessed");
+var show= document.getElementById("show");
 var userWins = 0;
 var userLoss= 0;
 var lettersGuessed= []; //hold each letter  
@@ -19,7 +20,6 @@ function setup(){
 computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)]; //pick letter randomly
 trial = 8;
 lettersGuessed = [];
-console.log(computerLetter);
 }
 
 function checkLetter(){ //run funtion to check answer
@@ -37,19 +37,26 @@ function checkLetter(){ //run funtion to check answer
                 //loop to hold the letters already guessed and display on screen
                 for(var i=0;i<lettersGuessed.length;i++){
                     temp = temp + " " + lettersGuessed[i] + ",";
-                    lettersGuessed_char.innerHTML = temp;
+                    lettersGuessed_char.innerHTML = temp.toUpperCase();
                 }
             }
                 if(trial === 0){ //restarts game if user runs out of chances
                  userLoss++;
-                lossesText.innerHTML = userLoss; //show in HTML
+                lossesText.innerHTML = userLoss;//update loss counter in HTML
+                show.innerHTML = computerLetter.toUpperCase(); //show in HTML
                 setup(); //re-run the game
                 }
+                
             }   
           
+            console.clear()
+
 document.onkeyup = function(event){ 
     userGuess = event.key; //store data as userGuess
-    checkLetter()
+    checkLetter();
+    console.log(userGuess);
+    
+    show.innerHTML = computerLetter.toUpperCase(); //show in HTML
     
         }
     
