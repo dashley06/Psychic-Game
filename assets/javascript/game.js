2223bb1c22d95f1a061f03928d42142b37eeb48f
Ashley Clark <debashley06@gmail.com>
2019-10-26 17:08:58 -0400

    fixed minor javascript errors, cleaned up css styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,64 @@
+$(document).ready(function() {
 
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var lettersGuessed_char= document.getElementById("lettersGuessed");
-var show= document.getElementById("show");
 var userWins = 0;
 var userLoss= 0;
 var lettersGuessed= []; //hold each letter  
 var trial = 8; //# of chances
 var computerChoices = ["a","b","c","d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerLetter = "";
+var computerLetter;
 var userGuess;
 
-//start game
-function setup(){
-computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)]; //pick letter randomly
-trial = 8;
-lettersGuessed = [];
 
-}
+setup();
 
-function checkLetter(){ //run funtion to check answer
+//run funtion to check answer
+function checkLetter(){ 
+
             if (userGuess === computerLetter){
                 userWins++; //increase win counter by 1
-                winsText.innerHTML = userWins;//change the HTML to display win
-                alert("Winner!");
-                setup(); //re-run the game
-            }
-            else{
-                trial--; //if the guess was wrong, decrease number of chances by 1
-                document.getElementById("trial").innerHTML = trial; //show remaining chances in HTML
+                $("#wins-text").text(userWins);
                 lettersGuessed.push(userGuess);
-                var temp = "";
-        
-                //loop to hold the letters already guessed and display on screen
-                for(var i=0;i<lettersGuessed.length;i++){
-                    temp = temp + " " + lettersGuessed[i] + ", ";
-                    lettersGuessed_char.innerHTML = temp.toUpperCase();
-                }
-            }
-                if(trial === 0){ //restarts game if user runs out of chances
-                 userLoss++;
-                lossesText.innerHTML = userLoss;//update loss counter in HTML
-                show.innerHTML = computerLetter.toUpperCase(); //show in HTML
-                alert("Sorry, you lost this time");
+                $("#lettersGuessed").text(lettersGuessed);
+                $("#show").text(`The correct answer was: ${computerLetter}`);
+                $("#alert").text("WINNER!! GOOD JOB!!!");
                 setup(); //re-run the game
-                }
-                
-            }   
-          
-           // console.clear()
 
+            } else {
+                trial--; //decrease number of chances by 1
+                $("#trial").text(trial);
+                lettersGuessed.push(userGuess);
+                var newArraySpaced = lettersGuessed.join(', ')
+                $("#lettersGuessed").text(newArraySpaced);
+                end();
+            }
+    
+}   
+
+//restart game
+function setup(){
+    computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)].toUpperCase(); //pick letter randomly
+    trial = 8;
+    lettersGuessed.length = 0;
+    console.log(computerLetter);
+    }
+    
+function end(){
+    if(trial === 0){ 
+        userLoss++;
+        $("#losses-text").text(userLoss);//update loss counter in HTML
+        $("#show").text(`The correct answer was: ${computerLetter}`);
+        $("#alert").text("Sorry, you lost this time.....Guess again to start a new game!");
+        setup(); //re-run the game
+
+}
+}
+          
 document.onkeyup = function(event){ 
-    userGuess = event.key; //store data as userGuess
+    userGuess = event.key.toUpperCase();
+    $("#show").text(" ");
+    $("#alert").text(" ");
     checkLetter();
-    console.log(userGuess);
-    
-    show.innerHTML = computerLetter.toUpperCase(); //show in HTML
-    
-        }
-    
+  
+}
+
+});
